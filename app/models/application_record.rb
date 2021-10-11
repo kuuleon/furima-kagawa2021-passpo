8135a038534b6eb491fb91f436141f31018cde0f
kuuleon <tudopassa427911@gmail.com>
2021-10-11 18:29:13 +0900

    新規登録画面までの遷移

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,8 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :family_name, :family_name_kana, :first_name_kana, :birthday])
+  end
 end
