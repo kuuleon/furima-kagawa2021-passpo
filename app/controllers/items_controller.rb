389bbdba76a24b3d8ed18f2d68a05c5635d56de2
kuuleon <tudopassa427911@gmail.com>
2021-10-20 18:02:02 +0900

    出品機能の実装（Java以外）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: :new
+
   def index
     @items = Item.all
   end
@@ -9,14 +11,29 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    if @item.save
-      redirect_to  new_item_path
+    if @item.save  #出品情報の保存に成功した場合、
+       redirect_to root_path #ルートパスに戻る
     else
-      @item.images.new
-      render :new
+      render :new #データが保存されなかったときは新規投稿ページへ戻る(newアクションのnew.html.erbを表示する)
     end
   end
 
   private
 
+  def item_params
+    params.require(:item).permit(:item_name, :explain, :category_id, :statement_id, :load_id, :prefecture_id, :delivery_days_id, :item_price, :image).merge(user_id: current_user.id)
+  end
+
 end
+
+# t.string      :item_name,          null: false
+# t.text        :explain,            null: false
+# t.integer     :item_price,         null: false
+# t.references  :user,               null: false, foreign_key: true
+
+# t.integer     :category_id,        null: false
+# t.integer     :statement_id,       null: false
+# t.integer     :load_id,            null: false
+# t.integer     :prefecture_id,      null: false
+# t.integer     :delivery_days_id,   null: false
+
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,18 +1,8 @@
 class Item < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :genre
   belongs_to :user
   has_one :purchase
   has_one_attached :image
 
-  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :delivery_days_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :load_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :statement_id, numericality: { other_than: 1 , message: "can't be blank"}
-
-  validates :item_price, numericality: { in: 300..9999999}
-  
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :load
@@ -20,28 +10,30 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :statement
 
+  #必須
+  with_options presence: true do
+    validates :image, :item_name, :explain, :user
+  end
+
+  #必須、ジャンルの選択が「--」の時は保存できないようにする
+  with_options presence: true, numericality: { other_than: 1, message: "can't be blank"} do
+    validates :category_id
+    validates :delivery_days_id
+    validates :load_id
+    validates :prefecture_id
+    validates :statement_id
+  end
+  
+
+  #価格は、¥300~¥9,999,999の間のみ且つ半角数値のみ保存可能
+  validates :item_price, numericality: { with: /\A[0-9]+\z/, message: 'Half-width number' }
+  validates :item_price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
+
+
 end
 
 
 
 
-#まだ
-# カテゴリーは、「---、メンズ、レディース、ベビー・キッズ、インテリア・住まい・小物、本・音楽・ゲーム、おもちゃ・ホビー・グッズ、家電・スマホ・カメラ、スポーツ・レジャー、ハンドメイド、その他」の11項目が表示されること（--- は初期値として設定すること）。
-# 商品の状態は、「---、新品・未使用、未使用に近い、目立った傷や汚れなし、やや傷や汚れあり、傷や汚れあり、全体的に状態が悪い」の7項目が表示されること（--- は初期値として設定すること）。
-# 配送料の負担は、「---、着払い(購入者負担)、送料込み(出品者負担)」の3項目が表示されること（--- は初期値として設定すること）。
-# 発送元の地域は、「---」と47都道府県の合計48項目が表示されること（--- は初期値として設定すること）。
-# 発送までの日数は、「---、12日で発送、23日で発送、4~7日で発送」の4項目が表示されること（--- は初期値として設定すること）。
 
 
-# #やった
-# 価格の情報が必須であること。
-# 価格は、¥300~¥9,999,999の間のみ保存可能であること。
-# 価格は半角数値のみ保存可能であること。
-# 商品名が必須であること。
-# 商品の説明が必須であること。
-# 商品画像を1枚つけることが必須であること。
-# カテゴリーの情報が必須であること。
-# 商品の状態の情報が必須であること。
-# 配送料の負担の情報が必須であること。
-# 発送元の地域の情報が必須であること。
-# 発送までの日数の情報が必須であること。
