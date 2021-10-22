b85c3f79704c7bd9cbba54f719ccccdf5c99c16e
sorani34 <sorani-info@sorani-sky.jp>
2021-10-22 20:31:29 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,8 +19,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
+    @item = Item.find(params[:id])
+    @item.destroy
     redirect_to root_path
   end
 
