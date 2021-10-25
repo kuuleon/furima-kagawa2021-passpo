be43e9f3d5a39db0d547ec8a83397828b76ec30d
hirokiss <astraea6514@icloud.com>
2021-10-25 23:43:33 +0900

    フォーム機能の設定

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -7,26 +7,27 @@ class PurchasesController < ApplicationController
 
   def create
     @purchase_address = PurchaseAddress.new(purchase_params)
-    if @address.valid?
-      @address.save
-      return redirect_to root_path
+    if @purchase_address.valid?
+      @purchase_address.save
+      redirect_to root_path
     else
-      render 'index'
+      render :new
     end
   end
 
   private
 
   def purchase_params
-    params.require(:donation_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_number, :tel_number).merge(user_id: current_user.id, item_id: item_id)
+    params.require(:donation_address).merge(:postal_code, :prefecture_id, :city, :house_number, 
+    :building_number, :tel_number, user_id: current_user.id, item_id: item_id, item_name: item_name, item_price: item_price )
   end
 
   # def address_params
   #   params.require(:address).permit(:postal_code, :prefecture_id, :city,:house_number, 
-  #                                   :building_number, :tel_number).merge(:purchase_id: @purchase.id)
+  #                                   :building_number, :tel_number).merge(purchase_id: purchase_id)
   # end
 
-  def pay_item
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-  end
+  # def pay_item
+  #   Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+  # end
 end
