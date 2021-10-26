eaa3af6e994ca59c115e39079b82c8dd8f7a4757
kuuleon <tudopassa427911@gmail.com>
2021-10-26 16:57:10 +0900

    バリデーションの修正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -12,7 +12,7 @@ class PurchasesController < ApplicationController
       @purchase_address.save
       redirect_to root_path
     else
-      render :new
+      render :index
     end
   end
 
--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,14 +1,23 @@
 class PurchaseAddress
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_number, :tel_number, :user_id, :item_id
-
+  
+  #必須
   with_options presence: true do
     validates :user_id
     validates :item_id
     validates :item_price
     validates :item_name
     validates :image
+    validates :city
+    validates :house_number
+    validates :tel_number, numericality: { only_integer: true }
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  end
+
+  # 必須、ジャンルの選択が「--」の時は保存できないようにする
+  with_options presence: true, numericality: { other_than: 1, message: "can't be blank" } do
+    validates :prefecture_id
   end
 
   def save
