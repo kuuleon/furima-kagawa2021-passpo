3310b6fc28c7730cfdf7addac8535757bd31b0f2
kuuleon <tudopassa427911@gmail.com>
2021-10-27 16:40:16 +0900

    テストコード修正2

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :item do
     item_name { Faker::Lorem.sentence }
-    image { Faker::Lorem.sentence }
+    #image { Faker::Lorem.sentence }
     explain { Faker::Lorem.sentence }
     association :user
     category_id           { 2 }
@@ -10,5 +10,9 @@ FactoryBot.define do
     prefecture_id         { 2 }
     delivery_days_id      { 2 }
     item_price            { 5000 }
+
+    # after(:build) do |item|
+    #   item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    # end
   end
 end
